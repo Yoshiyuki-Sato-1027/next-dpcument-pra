e909e96657413dadfb2d4a87ae8c829537c73f33
satou-yosiyuki <stuysyk1027@icloud.com>
2024-05-29 21:53:48 +0900

    Create Layout dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-white">{children}</div>;
+};
+
+export default DashboardLayout;
