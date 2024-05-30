51ac18e7bd626e1608ed72eea5bfd42c996003eb
satou-yosiyuki <stuysyk1027@icloud.com>
2024-05-30 20:59:37 +0900

    Practice usePathname

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const Page = () => {
+  const dashboardLink = "/dashboard";
+  const pathname = usePathname();
+  const isActive = pathname.startsWith(dashboardLink);
+  console.log("isActive", isActive);
   return <div>page</div>;
 };
 
-export default page;
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import Image from "next/image";
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const dashboardLink = "/dashboard";
+  const pathname = usePathname(); // 現在地の/を判定する
+  const isActive = pathname.startsWith(dashboardLink);
+  console.log("isActive", isActive);
   return (
     <>
-      <Link href="/dashboard">Dashboard</Link>
+      <Link
+        className={isActive ? "text-red" : "text-blue"}
+        href={dashboardLink}
+      >
+        Dashboard
+      </Link>
     </>
   );
 }
